ab1704db011b31d43ac7b59a8d6e8c4ce3b34cd8
NeonChrome93 <daarksky1919@gmail.com>
2024-09-15 19:23:25 +0300

    add eslint configuration

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ async function scrapeProduct(url, region) {
     const browser = await puppeteer.launch({
         headless: false,
         args: ['--start-maximized', '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-blink-features=AutomationControlled'],
-    })
-    const page = await browser.newPage()
+    });
+    const page = await browser.newPage();
 
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-    await page.setViewport({width: 1920, height: 1080})
-    await page.goto(url)
-    await page.waitForSelector('div[class^="ProductPage_informationBlock__"]', {timeout: 3000})
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    await page.setViewport({width: 1920, height: 1080});
+    await page.goto(url);
+    await page.waitForSelector('div[class^="ProductPage_informationBlock__"]', {timeout: 3000});
 
 
-    const regionTextElement = await page.$('div[class^="Region_region"]')
+    const regionTextElement = await page.$('div[class^="Region_region"]');
 
     const productName = url.match(/\/product\/([^\/]+)/)[1];
 
-    console.log('region', regionTextElement)
-    await regionTextElement.click()
+    console.log('region', regionTextElement);
+    await regionTextElement.click();
 
-    const buttons = await page.$$('li[class^="UiRegionListBase_item"]') //.find(elem => elem.textContent === region
+    const buttons = await page.$$('li[class^="UiRegionListBase_item"]'); //.find(elem => elem.textContent === region
     for (const button of buttons) {
         const text = await page.evaluate(el => el.textContent, button);
 
@@ -32,49 +32,50 @@ async function scrapeProduct(url, region) {
             break;
         }
     }
-    await new Promise((resolve, reject) => {
+    await new Promise((resolve) => {
         setTimeout(() => {
-            resolve()
-        }, 3000)
-    })
+            resolve();
+        }, 3000);
+    });
 
-    const modalCloseButton = await page.$('button[class^="Tooltip_closeIcon"]')
-    await modalCloseButton.click()
+    const modalCloseButton = await page.$('button[class^="Tooltip_closeIcon"]');
+    await modalCloseButton.click();
 
 
     await page.evaluate(() => {
         window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
     });
-    const cookieAgreeButton = await page.$('div[class^="CookiesAlert_agreeButton"]')
-    await cookieAgreeButton.click()
+    const cookieAgreeButton = await page.$('div[class^="CookiesAlert_agreeButton"]');
+    await cookieAgreeButton.click();
 
     await page.setViewport({
-        width: 2500, // Ширина страницы
-        height: 4000, // Высота страницы
+        width: 1500, // Ширина страницы
+        height: 3000, // Высота страницы
     });
-    const screenshot = await page.screenshot({path: `${productName}-${region}.jpg`, fullPage: false});
-    console.log(screenshot, " screenshot")
-    fs.writeFileSync("./screenshots/screenshot.jpg", screenshot,)
+    const screenshot = await page.screenshot({fullPage: false});
+    console.log(screenshot, ' screenshot');
+    
+    fs.writeFileSync(`./screenshots/${productName}-${region}.jpg`, screenshot,);
 
 
-    const priceOldSpan = await page.$('[class*="Price_role_old"]')
-    const priceOld = await (await priceOldSpan.getProperty('textContent')).jsonValue()
+    const priceOldSpan = await page.$('[class*="Price_role_old"]');
+    const priceOld = await (await priceOldSpan.getProperty('textContent')).jsonValue();
 
-    let priceDiscount
-    let priceCurrent
-    let rating
-    let  reviewCount
+    let priceDiscount;
+    let priceCurrent;
+    let rating;
+    let  reviewCount;
     if (priceOldSpan){
-        const priceDiscountSpan = await page.$('[class*="Price_role_discount"]')
-         priceDiscount = await (await priceDiscountSpan.getProperty('textContent')).jsonValue()
+        const priceDiscountSpan = await page.$('[class*="Price_role_discount"]');
+         priceDiscount = await (await priceDiscountSpan.getProperty('textContent')).jsonValue();
     } else {
         const priceCurrentSpan = await page.$('span[class*="Price_role_regular"]');
-        priceCurrent = await (await priceCurrentSpan.getProperty('textContent')).jsonValue()
+        priceCurrent = await (await priceCurrentSpan.getProperty('textContent')).jsonValue();
     }
-    let ratingClass = await page.$('[class^="ActionsRow_stars"]')
-    rating = await (await ratingClass.getProperty('textContent')).jsonValue()
-    let reviewCountClass = await page.$('[class^="ActionsRow_reviews"]:not([class*="ActionsRow_reviewsWrapper"]')
-    reviewCount = await (await reviewCountClass.getProperty('textContent')).jsonValue()
+    let ratingClass = await page.$('[class^="ActionsRow_stars"]');
+    rating = await (await ratingClass.getProperty('textContent')).jsonValue();
+    let reviewCountClass = await page.$('[class^="ActionsRow_reviews"]:not([class*="ActionsRow_reviewsWrapper"]');
+    reviewCount = await (await reviewCountClass.getProperty('textContent')).jsonValue();
 
 
     const productInfo = {
@@ -86,15 +87,14 @@ async function scrapeProduct(url, region) {
     };
 
 
-    const productData = `
-    price: ${productInfo.priceOld.split(" ")[0] || productInfo.priceCurrent.split(" ")[0] }
-    priceDiscount: ${productInfo.priceDiscount.split(" ")[0] || 'Нет скидочной цены'}
-     rating: ${productInfo.rating}
-     reviewCount: ${productInfo.reviewCount.split(" ")[0]}
-    `;
-    //console.log(productData, " productData")
+    const productData = `price=${productInfo.priceOld.split(' ')[0] || productInfo.priceCurrent.split(' ')[0] }\n`+
+`priceDiscount=${productInfo.priceDiscount.split(' ')[0] || 'Нет скидочной цены'}\n` +
+`rating=${productInfo.rating}\n` +
+`reviewCount=${productInfo.reviewCount.split(' ')[0]}\n`
+    ;
+    console.log(productData, ' productData');
 
-    fs.writeFileSync('product.txt', productData.trim());
+    fs.writeFileSync(`./text/${productName}-${region}.txt`, productData.trim());
 
     // Закрытие браузера
     await browser.close();
@@ -103,8 +103,8 @@ async function scrapeProduct(url, region) {
 const url = process.argv[2];
 const region = process.argv[3];
 
-console.log(url)
-console.log(region)
+console.log(url);
+console.log(region);
 
 // Запуск скрипта
 scrapeProduct(url, region)
